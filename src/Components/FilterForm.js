83bcee2fc90fc7f98bb563aa5a690a1e0e5fcfda
EidHachem <eidhachem1@gmail.com>
2022-10-19 19:06:01 +0300

    Add filter functionality

--- src/Components/FilterForm.js
+++ src/Components/FilterForm.js
@@ -1,49 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FilterForm = ({ filter, setFilter }) => {
+  const [all, setAll] = useState(true);
+  const [frontEnd, setFrontEnd] = useState(false);
+  const [fullStack, setFullStack] = useState(false);
   return (
-    <div class="main hidden lg:flex border rounded-full overflow-hidden m-4 select-none h-[3em] mb-16">
-      <div class="title w-[8em] flex justify-center items-center bg-blue text-beige font-semibold text-xl">
+    <div className="main hidden lg:flex border rounded-full overflow-hidden m-4 select-none h-[3em] mb-16">
+      <div className="title w-[8em] flex justify-center items-center bg-[#fff] text-blue font-bold text-xl border-r-2">
         Choose stack
       </div>
       <label
         htmlFor="all"
-        className="text-purple font-bold text-xl mr-5 flex radio py-2 w-[8em] justify-center items-center cursor-pointer border-r-2">
+        className={`text-blue font-bold text-xl flex radio py-2 w-[8em] justify-center items-center cursor-pointer border-r-2 ${
+          all && 'bg-blue text-beige'
+        }`}>
         <input
           type="radio"
           name="project"
           id="all"
-          className="my-auto transform scale-125"
+          className="hidden"
           value={filter}
-          onClick={(e) => setFilter('front-end' || 'full-stack')}
+          onClick={() => {
+            setFilter('front-end' || 'full-stack');
+            setAll(true);
+            setFrontEnd(false);
+            setFullStack(false);
+          }}
         />
-        <div class="title px-2">All</div>
+        <div className="title px-2">All</div>
       </label>
       <label
         htmlFor="front-end"
-        className="text-purple font-bold text-xl mr-5 flex radio py-2 w-[8em] justify-center items-center cursor-pointer border-r-2">
+        className={`text-blue font-bold text-xl flex radio py-2 w-[8em] justify-center items-center cursor-pointer border-r-2 ${
+          frontEnd && 'bg-blue text-beige'
+        }`}>
         <input
           type="radio"
           name="project"
           id="front-end"
-          className="my-auto transform scale-125"
+          className="hidden"
           value={filter}
-          onClick={(e) => setFilter('front-end')}
+          onClick={() => {
+            setFilter('front-end');
+            setFrontEnd(true);
+            setAll(false);
+            setFullStack(false);
+          }}
         />
-        <div class="title px-2">Front-end</div>
+        <div className="title px-2">Front-end</div>
       </label>
       <label
         htmlFor="full-stack"
-        className="text-purple font-bold text-xl mr-5 flex radio py-2 w-[8em] justify-center items-center cursor-pointer">
+        className={`text-blue font-bold text-xl flex radio py-2 w-[8em] justify-center items-center cursor-pointer ${
+          fullStack && 'bg-blue text-beige'
+        }`}>
         <input
           type="radio"
           name="project"
           id="full-stack"
-          className="my-auto transform scale-125"
+          className="hidden"
           value={filter}
-          onClick={(e) => setFilter('full-stack')}
+          onClick={() => {
+            setFilter('full-stack');
+            setFullStack(true);
+            setAll(false);
+            setFrontEnd(false);
+          }}
         />
-        <div class="title px-2">Full-stack</div>
+        <div className="title px-2">Full-stack</div>
       </label>
     </div>
   );
